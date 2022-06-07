712769958ab46f21c218b7546ec21580c5aaa621
Prof Edir <ecaramba@gmail.com>
2022-06-06 21:49:17 -0300

    aula de js

---
+++ aulajs.js
@@ -0,0 +1,46 @@
+// E -> P -> S
+
+// algoritmo
+
+var nome = "Edir"; // string
+var idade = 24; // number - integer
+var peso = 43.7; // Number - float
+var acordado = true; // boolean 
+
+// comentario 
+// alert("Olá " + nome + "!");
+// > - maior
+// < - menor
+// == - igual
+// >= - maior ou igual
+// <= - menor ou igual
+// != - diferente
+
+if ( idade > peso)
+{
+    console.log("verdade");
+} else
+{
+    console.log("falso");
+}
+
+// var texto = prompt("digite algo");
+
+// console.log(texto);
+
+// exec 1
+// peça 2 numeros e retorne o maior
+
+var num1 = prompt("Digite o num 1");
+var num2 = prompt("Digite o num 2");
+
+if (num1 > num2)
+{
+    console.log(num1)
+} else 
+{
+    console.log("O manior numero e: " + num2)
+}
+
+// exec 2
+// peça 3 numeros e dizer o maior e tmb o menor
