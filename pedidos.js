abeb591dc3175abdc612a06809358df110ecaf13
Edir Dumaszak <ecaramba@gmail.com>
2022-06-14 21:00:02 -0300

    input mask

--- pedidos.js
+++ pedidos.js
@@ -20,3 +20,35 @@ $("#bt-adicionar").click(function ()
 
 }); // fim do click
 
+$("#bt-confirmar").click(function(){
+    
+    var nome = $("#nome-cliente").val();
+    var email = $("#email").val();
+    var cpf = $("#cpf").val();
+    var telefone = $("#telefone").val();
+
+    console.log(nome, email, cpf, telefone);
+
+    $("#nome-cliente, #email, #telefone").removeClass('is-invalid');
+
+    if (nome == "" )
+    {
+        $("#nome-cliente").addClass('is-invalid');
+    }
+
+    if (email == "")
+    {
+        $("#email").addClass('is-invalid');
+    }
+
+    if (telefone == "")
+    {
+        $("#telefone").addClass('is-invalid');
+    }
+
+})
+
+$(document).ready(function(){
+    $("#cpf").mask("000.000.000-00");
+});
+
